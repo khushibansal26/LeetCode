3c2dd5c282ffdb44c9fdcee874554ae6262136a5
Khushi Bansal <84730669+khushibansal26@users.noreply.github.com>
2022-06-11 11:20:56 +0530

    Time: 3 ms (80.03%), Space: 8 MB (77.24%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-       unordered_map<int,int> m;
-        for(auto i:arr)
-            m[i]++;
-       set<int> st;// set stores unique occurance only of each frq of number
-       for(auto it:m)
-       {
-           if(st.count(it.second)>0)  // if count() checks if particluar value is repeated or not..
-               // if it is already present returns 1 else 0
-               return false;
-           else
-               st.insert(it.second);
-       }
+        int ans;
+        unordered_map<int,int> m;
+        for(auto it:arr){
+            m[it]++;
+        }
+        set <int> s;
+        for(auto i: m){
+            if(s.count(i.second)>0)
+                return false;
+            else
+                s.insert(i.second);
+        }
         return true;
+        
     }
 };
