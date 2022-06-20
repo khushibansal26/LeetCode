3b1f11dd1629ade8fb63917432a06e4623afbee4
Khushi Bansal <84730669+khushibansal26@users.noreply.github.com>
2022-06-20 11:00:22 +0530

    Time: 4 ms (69.58%), Space: 11.2 MB (29.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
-    int getPivot ( vector <int>  &nums){
-        int n  = nums.size();
-        int lo=0,hi = n-1;
-        int mid;
-         while(lo<hi){
-          
-            mid =(lo + hi)/2;
-            if(nums[mid]>=nums[0]){
-                lo = mid + 1;  
-            }
-            else{
-                hi = mid;
-            }
-        }
-        return lo;
-    }
-    int search(vector<int>& nums, int target) {
-        int n=nums.size();
-        int ans=-1;
-       int pivot = getPivot(nums);
-      
-        if(target >=nums[pivot]  && target<=nums[n-1])
-        { int lo=pivot,hi = n-1;
-        int mid;
-         while(lo<=hi){
-             mid =(lo + hi)/2;
-             if(nums[mid]==target)
-             {ans =mid;
-             break;}
-             else if(nums[mid]>target)
-                 hi = mid-1;
-             else
-                 lo =mid +1;
-             
-         }
-            
-        }
-        else{
-            int lo=0,hi = pivot-1;
-        int mid;
-         while(lo<=hi){
-             mid =(lo + hi)/2;
-             if(nums[mid]==target)
-             {ans =mid;
-             break;}
-             else if(nums[mid]>target)
-                 hi = mid-1;
-             else
-                 lo =mid +1;
-            
-        }
-        }
-         return ans;
-    }
+    int search(vector<int>& a, int target) {
+        int n=a.size();
+        int lo=0,hi=n-1;
+      while(lo<=hi){
+      int   mid =lo + (hi-lo)/2;
+          if(a[mid]==target)
+              return mid;
+        
+          //left side is sorted
+        if(a[mid]>=a[lo])  {
+              if(target>=a[lo]&&target<=a[mid])
+              hi=mid-1;
+          else
+              lo=mid+1;
+          }
+          else{
+              if(target>=a[mid] &&target<=a[hi])
+                  lo=mid+1;
+              else
+                  hi=mid-1;
+          }
        
+          
+          
+      }
+      
+           return -1;
+          
+    }
 };
