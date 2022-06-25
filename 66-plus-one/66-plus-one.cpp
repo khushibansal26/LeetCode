c6648641aa749006b491cf05089377a25740def2
Khushi Bansal <84730669+khushibansal26@users.noreply.github.com>
2022-06-25 16:28:33 +0530

    Time: 7 ms (18.23%), Space: 8.7 MB (61.35%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -3,37 +3,20 @@ public:
     vector<int> plusOne(vector<int>& digits) {
        
      int n =digits.size();
-        int cnt=0;
-        for(auto it:digits){
-            if (it==9)
-                cnt++;
-                
+     for(int i=n-1;i>=0;i--){
+         if(digits[i]==9)
+             digits[i]=0;
+        else {
+            digits[i]=digits[i]+1;
+            return digits;
         }
-        if(n==cnt){
-            vector<int>ans;
-            ans.push_back(1);
-            for(int i=1;i<=n;i++)
-                ans.push_back(0);
-            return ans;
+     }
+        if(digits[0]==0)
+        {digits[0]=1;
+         digits.push_back(0);
         }
-       vector<int>ans=digits;
-        if(digits[n-1]!=9)
-        {
-            ans[n-1]=ans[n-1]+1;
-            return ans;
-        }
-        else{
-            int i;
-           for(i=n-1;i>=0;i--){
-               if(ans[i]==9)
-                   ans[i]=0;
-               else
-                   break;
-                   
-           }
-            ans[i]=ans[i]+1;
-        }
-        return ans;
+        return digits;
+        
        
       
     }
