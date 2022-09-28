58befbbf71e7303b029ca3cc40dfcfee2691d680
Khushi Bansal <84730669+khushibansal26@users.noreply.github.com>
2022-09-28 19:49:20 +0530

    Time: 3 ms (75.79%), Space: 7.1 MB (70.49%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> vec;
-        vector<vector<int>> ans;
-        int powsize = pow(2,n);  // here n= 3, powsize= 8
-        for(int counter =0;counter<powsize;counter++){
-//            counter= 1
-//             counter=2
-            
-//             counter= 5 => 101 =>[1,3]  => we check the bit is set or not
-            // .....   counter=7
-            for(int j=0;j<n;j++){
-                if((1<<j) & counter)
-                  vec.push_back(nums[j]);  
-            }
-            ans.push_back(vec);
-            vec.clear();
+ void subseq(int ind,vector<int>&temp,vector<vector<int>> &ans,vector<int> &nums){
+        if(ind == nums.size()){
+            ans.push_back(temp);
+            return ;
         }
+        
+        temp.push_back(nums[ind]);
+        subseq(ind +1, temp,ans,nums);
+        temp.pop_back();
+        subseq(ind+1,temp,ans,nums);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n =nums.size();
+        vector<vector<int>> ans;
+        vector<int> temp;
+        subseq(0,temp,ans,nums);
         return ans;
     }
+    
+   
+    
 };
